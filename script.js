b9f866892588b9a2e59fc14eadaf845e5e1f6f56
Prathambattamwar <88773238+Prathambattamwar@users.noreply.github.com>
2024-06-07 12:53:08 +0000

    print

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-var mysql=require('mysql');
-
-var con =mysql.createConnection({
-    host:"localhost",
-    user: "yourusername",
-    password: "password"
-}).listen(8080);
-
-con.connect(function(err){
-    if(err) throw err;
-    console.log("Connected!");
-    con.query("CREATE DATABASE Mmydb",function (err, result) {
-        if(err) throw err;
-        console.log("database created");
-    });
-});
+console.log("Pratham")
