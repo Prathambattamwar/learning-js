a0726cb75c4c1d2d99d5bdc3d57cd5fdd7c4718d
Prathambattamwar <88773238+Prathambattamwar@users.noreply.github.com>
2024-06-08 14:24:08 +0000

    data types and variables

---
+++ basics/Data_type.js
@@ -0,0 +1,23 @@
+"use strict" //treat all JS code as newer version
+
+//alert(3+3) //we are using node.js ,not browser
+console.log(3+
+    3) //code readability should be high
+
+console.log("pratham")
+
+let name="Pratham"
+let age=34
+let isLoggedIn =false
+
+//number=>2to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value(value pata nahi hai)
+// undefined=> 
+//symbol =>unique
+
+// object
+console.log(typeof"Pratham")
+console.log(typeof null)//object
---
+++ basics/variable.js
@@ -0,0 +1,15 @@
+const accountId= 24255
+let accountEmail = "prathambattamwar@gmail.com"
+var accountPass = "12345"
+accountCity = "jaipur"
+
+// accountId = 2 //not allowed
+/*
+Prefer not to  use var
+ because of issue in block scope and functional scope
+*/
+accountEmail="abdsa.com"
+accountPass="21312"
+accountCity="mangular"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPass,accountCity]);
