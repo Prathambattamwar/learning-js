9849eb811dbc2168c1dc0b0fda4e746e18a9e956
Prathambattamwar <88773238+Prathambattamwar@users.noreply.github.com>
2024-06-08 14:38:39 +0000

    conversionOperation

---
+++ basics/conversionOperation.js
@@ -0,0 +1,19 @@
+let score =false
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber= Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "45" => 45
+//"34erw" => NaN
+// true=> 1; false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+let someNumber= 6
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
