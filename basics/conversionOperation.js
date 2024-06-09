c87f05283c2794ced61d63e3350799eb1bfb9fcf
Prathambattamwar <88773238+Prathambattamwar@users.noreply.github.com>
2024-06-09 04:57:19 +0000

    comparision and operation

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,19 +1,38 @@
-let score =false
-//console.log(typeof score);
-//console.log(typeof(score));
+// let score =false
+// //console.log(typeof score);
+// //console.log(typeof(score));
 
-let valueInNumber= Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+// let valueInNumber= Number(score)
+// //console.log(typeof valueInNumber);
+// //console.log(valueInNumber);
 
-// "45" => 45
-//"34erw" => NaN
-// true=> 1; false=>0
+// // "45" => 45
+// //"34erw" => NaN
+// // true=> 1; false=>0
 
-let isLoggedIn=1
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-let someNumber= 6
-let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// let someNumber= 6
+// let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+//**********************Operation******************* */
+
+// console.log("1" +2);
+// console.log(1+"2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3+4)* 5 % 3);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversio
